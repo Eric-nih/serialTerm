cc126fe6deeb645b055ffff495e0f990c5b3d621
Eric Bennett <bennette@nhlbi.nih.gov>
2026-05-22 13:52:33 -0400

    added comments

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Kohzu Serialport Terminal Program
 This is a simple program to enter text commands to be sent via serial port to a Kohzu SC-410 or SC-210 controller.
 It assumes that you are using COM4.  Change this in code if you use a different port.
 Type 'exit' or 'EXIT' to quit
--- prepCommand.py
+++ prepCommand.py
@@ -1,5 +1,5 @@
 def prepCommand(str):
-    output = bytearray(b'\x02')
+    output = bytearray(b'\x02') # ASCII STX character. This is needed by the controller
     output += bytearray(str,"utf-8")
-    output += bytearray('\r\n',"utf-8")
+    output += bytearray('\r\n',"utf-8") # the controller needs both the carriage return and line feed characters
     return output                 
